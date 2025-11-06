5f6922aa684b3a51668c4bdbad7db85c48f267c4
Mafer_Hernandez <hernandezmf05@hotmail.com>
2025-11-06 15:34:04 -0500

    feat se crea ejemplo de lista

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,36 @@
 package org.example;
 
+import com.google.protobuf.LazyStringArrayList;
+
+import java.sql.Array;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        /*//1.variables primitivas
+        int precio=5000;
+        double peso=2.5;
+        boolean estaPodrida= false;
+
+        //2.variables tipo envoltura
+        String nombre="Juan";*/
+
+        //Manzana
+        String nombre="Manzana";
+        Integer precio=5000;
+        String descripcion= "Deliciosa manzana importada";
+        Integer codigo= 4587986;
+
+        //Arreglo primitivo
+        String[] frutas= new String[5]; // el numero es la cantidad de posiciones
+
+       //
+        ArrayList<String> frutasDos= new ArrayList<>();
+        frutasDos.add("Manzana");
+        frutasDos.add("Pera");
+        frutasDos.add("Sandia");
+        System.out.println(frutasDos);
+
     }
 }
